cbd079be573b4f1d374f5d01a8d56928020776ca
AjinkyaP-09 <pameajinkya09@gmail.com>
2025-09-06 09:15:35 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Azure Container Apps! 🚀"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
